7679ae44945fc2e5e8df088ee61b570098c28f28
shubh05 <aggarwal.shubham@gmail.com>
2025-12-23 16:25:35 +0530

    1.0

--- schedule_alerts.py
+++ schedule_alerts.py
@@ -1,6 +1,7 @@
+import time
 import pandas as pd
 from datetime import datetime, timedelta
-import asyncio,os, telegram
+import asyncio, telegram
 from telegram.constants import ParseMode
 
 def send_telegram(grp, token_str, msg):
@@ -72,51 +73,12 @@ df['End_Datetime'] = df.apply(lambda x: parse_datetime(x['End Date'], x['End Tim
 # --- 3. GENERATE ALERTS ---
 now = datetime.now()
 today_date = now.date()
+
+# print(f"\nReport Generated: {now.strftime('%d-%m-%Y %I:%M %p')}\n")
+
+
+# ALERT 1: State Wise Ongoing/Future
 message = ''
-
-print(f"\nReport Generated: {now.strftime('%d-%m-%Y %I:%M %p')}\n")
-
-# ALERT 1: Activities Starting Today
-
-starting_today = df[(df['Start_Datetime'].dt.date == now.date())]
-# print("🔔 *ACTIVITIES Starting Today*")
-message = message + "🔔 *ACTIVITIES Starting Today*\n\n"
-
-if starting_today.empty:
-    # print("*No activities starting Today.*")
-    message = message + "*No activities starting Today.*"
-else:
-    for _, row in starting_today.iterrows():
-        # print(row)
-        start_str = row['Start_Datetime'].strftime('%I:%M %p')
-        if start_str == '12:00 AM':
-            start_str = ''
-        else:
-            start_str = '*At* ' + start_str
-
-        # print(f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Starts On*: {row['Start Date']} {start_str}\n")
-        message = message + f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Starts On*: {row['Start Date']} {start_str}\n"
-
-
-# ALERT 2: Activities Ending Within 24 Hours
-limit_24h = now + timedelta(hours=24)
-ending_today = df[(df['End_Datetime'] <= limit_24h) & (df['End_Datetime'] > now)]
-# print("\n\n🔔 *ACTIVITIES ENDING in 24 Hours*")
-message = message + "\n\n🔔 *ACTIVITIES ENDING in 24 Hours*\n\n"
-
-if ending_today.empty:
-
-    # print("*No activities ending in next 24 hours.*")
-    message = message + "*No activities ending in next 24 hours.*"
-else:
-    for _, row in ending_today.iterrows():
-        # print(row)
-        end_str = row['End_Datetime'].strftime('%I:%M %p')
-        # print(f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Ends At*: {row['End Date']} {end_str}\n")
-        message = message + f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Ends At*: {row['End Date']} {end_str}\n"
-
-
-# ALERT 3: State Wise Ongoing/Future
 # print("\n\n📅 *STATE-WISE ACTIVITY STATUS*")
 message = message + "\n\n\n📅 *STATE-WISE ACTIVITY STATUS*\n"
 # Filter: End Date must be in the future (or present)
@@ -136,6 +98,76 @@ for state, group in active_df.groupby('State'):
         message = message + f"- \[*{row['Status']}*] {row['Activity']}\n- *From*: {start_disp}  *To*: {end_disp}\n\n"
 
 
-print(message)
+# print(message)
 
 send_telegram('@VS_Notices', '7873667251:AAFoZVUhEM5cbLsvCTrpuJ6BxJy-WmvWY14', message)
+
+# ALERT 2: Activities Starting Today
+message = ''
+starting_today = df[(df['Start_Datetime'].dt.date == now.date())]
+# print("🔔 *ACTIVITIES Starting Today*")
+message = message + "🔔 *ACTIVITIES Starting Today*\n\n"
+
+if starting_today.empty:
+    # print("*No activities starting Today.*")
+    message = message + "*No activities starting Today.*"
+else:
+    for _, row in starting_today.iterrows():
+        # print(row)
+        start_str = row['Start_Datetime'].strftime('%I:%M %p')
+        if start_str == '12:00 AM':
+            start_str = ''
+        else:
+            start_str = '*At* ' + start_str
+
+        # print(f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Starts On*: {row['Start Date']} {start_str}\n")
+        message = message + f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Starts On*: {row['Start Date']} {start_str}\n"
+
+send_telegram('@VS_Notices', '7873667251:AAFoZVUhEM5cbLsvCTrpuJ6BxJy-WmvWY14', message)
+
+while 1:
+    df = pd.read_excel(file_path)
+
+    # --- 1. FORMAT VALIDATION ---
+    date_pattern = r'^\d{2}-\d{2}-\d{4}$'
+    # Regex for 12-hour format (e.g., 2:00 pm, 11:00 AM)
+    time_pattern_12h = r'^(0?[1-9]|1[0-2]):[0-5][0-9]\s?(AM|PM|am|pm)$'
+    time_pattern_24h = r'^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$'
+
+    print(f"Format Check - Date (DD-MM-YYYY): {'Passed' if check_format(df['Start Date'], date_pattern) else 'Failed'}")
+    print(
+        f"Format Check - Time : {'Passed' if check_format(df['Start Time'], time_pattern_24h) else 'Failed (Using flexible parser)'}")
+
+    # --- 2. DATA PREPARATION ---
+    # Fill Missing Times with defaults
+    df['Start Time'] = df['Start Time'].fillna('00:00')
+    df['End Time'] = df['End Time'].fillna('11:59 PM')
+
+    df['Start_Datetime'] = df.apply(lambda x: parse_datetime(x['Start Date'], x['Start Time']), axis=1)
+    df['End_Datetime'] = df.apply(lambda x: parse_datetime(x['End Date'], x['End Time'], is_end=True), axis=1)
+
+    # --- 3. GENERATE ALERTS ---
+    now = datetime.now()
+    today_date = now.date()
+
+    # ALERT 3: Activities Ending Within 24 Hours
+    message = ''
+    limit_24h = now + timedelta(hours=24)
+    ending_today = df[(df['End_Datetime'] <= limit_24h) & (df['End_Datetime'] > now)]
+    # print("\n\n🔔 *ACTIVITIES ENDING in 24 Hours*")
+    message = message + "\n\n🔔 *ACTIVITIES ENDING in Next 24 Hours*\n\n"
+
+    if ending_today.empty:
+
+        # print("*No activities ending in next 24 hours.*")
+        message = message + "*No activities ending in next 24 hours.*"
+    else:
+        for _, row in ending_today.iterrows():
+            # print(row)
+            end_str = row['End_Datetime'].strftime('%I:%M %p')
+            # print(f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Ends At*: {row['End Date']} {end_str}\n")
+            message = message + f"- *State - {row['State']} - {row['Round']} Round*\n- *Activity* - {row['Activity']}\n- *Ends At*: {row['End Date']} {end_str}\n"
+
+    send_telegram('@VS_Notices', '7873667251:AAFoZVUhEM5cbLsvCTrpuJ6BxJy-WmvWY14', message)
+    time.sleep(3600)
+    del df
