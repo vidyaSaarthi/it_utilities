d4a01d1aa62d07ba73544e79d43de6abce30807d
shubh05 <aggarwal.shubham@gmail.com>
2026-04-03 16:42:30 +0530

    1.0

--- google_alert_rss_to_telegram.py
+++ google_alert_rss_to_telegram.py
@@ -4,16 +4,26 @@ import time
 
 # Configuration
 RSS_URL_LIST = [
-"https://www.google.co.in/alerts/feeds/06660699284050266053/9479878068789660271",
-"https://www.google.co.in/alerts/feeds/06660699284050266053/3248098847498370551",
-"https://www.google.co.in/alerts/feeds/06660699284050266053/1774105227142705003",
-"https://www.google.co.in/alerts/feeds/06660699284050266053/16270256872332916796",
+"https://www.google.co.in/alerts/feeds/06660699284050266053/9479878068789660271", #BITSAT
+"https://www.google.co.in/alerts/feeds/06660699284050266053/3248098847498370551", #COMEDK
+"https://www.google.co.in/alerts/feeds/06660699284050266053/1774105227142705003", #JEE
+"https://www.google.co.in/alerts/feeds/06660699284050266053/16270256872332916796", #NEET
 "https://www.google.com/alerts/feeds/06660699284050266053/12485566338060611844", #UGEE
 "https://www.google.com/alerts/feeds/06660699284050266053/12485566338060613940", #JAC Delhi
 "https://www.google.com/alerts/feeds/06660699284050266053/9346545140038872930", #MBBS
 "https://www.google.com/alerts/feeds/06660699284050266053/10620334520593906679", #VITEEE
 "https://www.google.com/alerts/feeds/06660699284050266053/14453981990149627571", #SITEEE
-"https://www.google.com/alerts/feeds/06660699284050266053/13147617054575588815" #MET Manipal
+"https://www.google.com/alerts/feeds/06660699284050266053/13147617054575588815", #MET Manipal
+"https://www.google.com/alerts/feeds/06660699284050266053/10438470197552500086" #CUET
+"https://www.google.com/alerts/feeds/06660699284050266053/5387349637885216539", #NISER
+"https://www.google.com/alerts/feeds/06660699284050266053/1287242344672731619", #IISER
+"https://www.google.com/alerts/feeds/06660699284050266053/3680806040444127722" #DAICT
+"https://www.google.com/alerts/feeds/06660699284050266053/5213746017576248862", #JOSAA
+"https://www.google.com/alerts/feeds/06660699284050266053/11662474502245257093", #JIIT
+"https://www.google.com/alerts/feeds/06660699284050266053/4933146280845086354", #JAYPEE INSTITUTE OF INFORMATION TECHNOLOGY
+"https://www.google.com/alerts/feeds/06660699284050266053/15015340571517855658", #HSTES
+"https://www.google.com/alerts/feeds/06660699284050266053/15482280052394751531", #JAC Chandigarh
+"https://www.google.com/alerts/feeds/06660699284050266053/15482280052394752618", #SRMJEE
 ]
 
 BOT_TOKEN = "8526202388:AAG5bD6MSaHBh1Fzk042J5cYYmcC-PwgD84"
